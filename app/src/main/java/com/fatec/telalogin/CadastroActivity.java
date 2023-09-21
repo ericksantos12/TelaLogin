1e9b41829f0ac5b5e89e1882837f8637e4236c10
GabrielleCGNeves <gabrielecarvalhoneves@gmail.com>
2023-09-21 14:11:27 -0300

    :tada: first commit
    Co-authored-by: Erick Santos <erick.sousa@hotmail.com.br>

---
+++ app/src/main/java/com/fatec/telalogin/CadastroActivity.java
@@ -0,0 +1,14 @@
+package com.fatec.telalogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_layout);
+    }
+}
